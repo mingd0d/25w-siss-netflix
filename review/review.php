2f9e92f650fa336b1f1d7b2a1437306fc5c74de5
jdain1 <24dd1301@sookmyung.ac.kr>
2025-02-12 00:05:33 +0900

    Update review.php

--- review/review.php
+++ review/review.php
@@ -44,7 +44,7 @@ $result = $conn->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>리뷰 페이지</title>
-    <link rel="stylesheet" href="../s.css">
+    <link rel="stylesheet" href="../css/s.css">
 </head>
 <style>
     h1{
