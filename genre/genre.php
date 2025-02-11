58a1760ba9eaf48961497420a3ac17f7a3996887
jdain1 <24dd1301@sookmyung.ac.kr>
2025-02-12 00:03:23 +0900

    Update genre.php

--- genre/genre.php
+++ genre/genre.php
@@ -22,7 +22,7 @@ while ($row = mysqli_fetch_assoc($result)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>장르선택</title>
-    <link rel="stylesheet" href="../s.css">
+    <link rel="stylesheet" href="../css/s.css">
 </head>
 <style>
     img{
