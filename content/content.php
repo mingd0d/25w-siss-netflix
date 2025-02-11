3d3971f7cc99f10f4d8c0715b23207cb8ce93d4a
jdain1 <24dd1301@sookmyung.ac.kr>
2025-02-12 00:04:22 +0900

    Update content.php

--- content/content.php
+++ content/content.php
@@ -78,7 +78,7 @@ if (array_key_exists($genre, $contents)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>콘텐츠</title>
-    <link rel="stylesheet" href="../s.css">
+    <link rel="stylesheet" href="../css/s.css">
 </head>
 <style>
     img{
